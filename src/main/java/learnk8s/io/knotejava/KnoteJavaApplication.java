8a50718f9ca2ed4c2ad327f86ee64ecc3a54e0bd
Toan Nguyen <tnguyen@us.maxnerva.com>
2024-03-31 20:41:53 +0700

    int project

---
+++ src/main/java/learnk8s/io/knotejava/KnoteJavaApplication.java
@@ -0,0 +1,13 @@
+package learnk8s.io.knotejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoteJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoteJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learnk8s/io/knotejava/KnoteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package learnk8s.io.knotejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
